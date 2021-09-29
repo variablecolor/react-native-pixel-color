67052bcd887a8d872068b2dacdfa44ef7d22f5fe
Corey Mann <corey.mann@variableinc.com>
2021-09-29 11:47:45 -0400

    Upgrades project to support androidx, and adds more flexibility to consumer for specifying android versions.

--- android/src/main/java/fr/bamlab/rnimageresizer/ImageResizer.java
+++ android/src/main/java/fr/bamlab/rnimageresizer/ImageResizer.java
@@ -10,8 +10,8 @@ import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.Base64;
 import android.util.Pair;
 
